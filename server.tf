a13c0af270efc69f2bf45f2608353381b75a108d
MAZYAFAI <90369169+MAZYAFAI@users.noreply.github.com>
2022-12-08 15:44:04 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -2,7 +2,7 @@ provider "aws" {
 region = "ap-south-1"
 }
 resource "aws_instance" "myawsserver" {
-  ami = "ami-0b0dcb5067f052a63"
+  ami = "ami-074dc0a6f6c764218"
   instance_type = "t2.micro"
   key_name = "raman-import"
 

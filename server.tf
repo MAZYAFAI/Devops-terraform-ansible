e21709dec45f2970cc76714651d00ce85d73f8a6
MAZYAFAI <90369169+MAZYAFAI@users.noreply.github.com>
2022-12-18 03:07:52 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -2,7 +2,7 @@ provider "aws" {
 region = "ap-south-1"
 }
 resource "aws_instance" "myawsserver" {
-  ami = "ami-074dc0a6f6c764218"
+  ami = "ami-0cca134ec43cf708f"
   instance_type = "t2.micro"
   key_name = "cicd-import"
 
